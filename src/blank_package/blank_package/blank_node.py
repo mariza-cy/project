189d026918beb25b5bdf401de7db01f651fb4acf
Mariza Paspalli <mariza@Paspallis-MacBook-Air.local>
2026-01-21 10:10:49 +0200

    IDK x2

--- src/blank_package/blank_package/blank_node.py
+++ src/blank_package/blank_package/blank_node.py
@@ -12,14 +12,18 @@ class Blinker(Node):
         self.vehicle_name = os.getenv('VEHICLE_NAME')
         self.publisher = self.create_publisher(LEDPattern, f'/{self.vehicle_name}/led_pattern', 1)
         self.timer = self.create_timer(1, self.change_color)
-        counter = 0
+        self.counter = 0
 
     def change_color(self):
         msg = LEDPattern()
-        if counter%2==0: pattern = ColorRGBA(r=0.0, g=0.0, b=1.0, a=1.0)
+        
+        if self.counter%2==0: pattern = ColorRGBA(r=0.0, g=0.0, b=1.0, a=1.0)
         else: pattern = ColorRGBA(r=1.0, g=1.0, b=0.0, a=1.0)
+
         msg.rgb_vals = [pattern] * 5
         self.publisher.publish(msg)
+
+        self.counter+=1
 
 def main():
     rclpy.init()
