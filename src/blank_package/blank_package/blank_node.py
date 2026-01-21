127463e619fc97eee1af9e9d3541424d9dfd1b14
Mariza Paspalli <mariza@Paspallis-MacBook-Air.local>
2026-01-21 09:14:28 +0200

    Added code to make LEDs green

--- src/blank_package/blank_package/blank_node.py
+++ src/blank_package/blank_package/blank_node.py
@@ -9,29 +9,15 @@ from std_msgs.msg import ColorRGBA
 class Blinker(Node):
     def __init__(self):
         super().__init__('blinker')
-        self.vehicle_name = os.getenv('VEHICLE_NAME')
+        self.vehicle_name = os.getenv('duckie04')
         self.publisher = self.create_publisher(LEDPattern, f'/{self.vehicle_name}/led_pattern', 1)
-        self.counter = 0
-        self.timer = self.create_timer(1, self.publish_pattern)
+        self.timer = self.create_timer(1, self.change_colour)
 
-    def publish_pattern(self):
-        # LEDPattern is a custom Duckietown Message
+    def change_colour(self):
         msg = LEDPattern()
-
-        if self.counter % 3 == 0:
-            # ColorRGBA is a standard message of ROS2
-            pattern = ColorRGBA(r=1.0, g=0.0, b=0.0, a=1.0)
-        elif self.counter % 3 == 1:
-            pattern = ColorRGBA(r=0.0, g=1.0, b=0.0, a=1.0)
-        else:
-            pattern = ColorRGBA(r=0.0, g=0.0, b=1.0, a=1.0)
-
-        # 5 Leds to fill
+        pattern = ColorRGBA(r=0.0, g=0.0, b=1.0, a=1.0)
         msg.rgb_vals = [pattern] * 5
-
         self.publisher.publish(msg)
-        self.counter += 1
-
 
 def main():
     rclpy.init()
