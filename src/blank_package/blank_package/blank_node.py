c47992f7c352d38d39863fde70174bf60a298b7c
Mariza Paspalli <mariza@Paspallis-MacBook-Air.local>
2026-01-21 09:47:53 +0200

    Hopefully it will work now

--- src/blank_package/blank_package/blank_node.py
+++ src/blank_package/blank_package/blank_node.py
@@ -9,11 +9,11 @@ from std_msgs.msg import ColorRGBA
 class Blinker(Node):
     def __init__(self):
         super().__init__('blinker')
-        self.vehicle_name = os.getenv('duckie04')
+        self.vehicle_name = os.getenv('VEHICLE NAME')
         self.publisher = self.create_publisher(LEDPattern, f'/{self.vehicle_name}/led_pattern', 1)
-        self.timer = self.create_timer(1, self.change_colour)
+        self.timer = self.create_timer(1, self.change_color)
 
-    def change_colour(self):
+    def change_color(self):
         msg = LEDPattern()
         pattern = ColorRGBA(r=0.0, g=0.0, b=1.0, a=1.0)
         msg.rgb_vals = [pattern] * 5
